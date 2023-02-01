25b348ac356613fa54a149b3555516f0a1aec1d7
VadimTalimonchik <tvadmins@gmail.com>
2023-02-01 20:04:41 +0300

    запись в файл2

--- lec_2.py
+++ lec_2.py
@@ -1,4 +1,11 @@
+# colors = ['red ', 'green ', 'blue2 ']
+# data = open('file.txt', 'a')
+# data.writelines(colors) # разделителей небудет
+# data.close()
+
 colors = ['red ', 'green ', 'blue2 ']
 data = open('file.txt', 'a')
 data.writelines(colors) # разделителей небудет
+data.write('\nLINE 12\n')
+data.write('LINE 13\n')
 data.close()
