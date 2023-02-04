eb967078f178dc04cacb1c354792e3029950a56f
VadimTalimonchik <tvadmins@gmail.com>
2023-02-04 10:41:41 +0300

    использование модуля из другого файла

--- lec_2.py
+++ lec_2.py
@@ -18,8 +18,28 @@
 #     data.write('line 22\n')
 
 # чтение из файла
-path = 'file.txt'
-data = open(path, 'r')
-for line in data:
-    print(line)
-data.close()
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# ФУНКЦИИ И МОДУЛИ
+
+# import hello
+# print(hello.f(1))
+
+# import hello as h
+# print(h.f(2.3))
+
+# Кортеджи
+
+a, b = 2, 6
+print(a, b)
+
+a = (2, 6)
+print(a)
+print(a[0])  
+
+
+
